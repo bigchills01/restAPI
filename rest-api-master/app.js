fb551a8e8c40377987600a715bd7487dfad0a0d6
bigchills01 <ezechidera90@gmail.com>
2023-07-13 17:33:12 +0100

    restapi

--- rest-api-master/app.js
+++ rest-api-master/app.js
@@ -1,18 +1,19 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+require('dotenv').config({ path: './config/.env' });
+const express = require('express');
+const app = express();
+const port = 3010;
+const { MongoClient } = require('mongodb');
 
-mongoose.connect(process.env.DATABASE_URL)
-const db = mongoose.connection
-db.on('error', (error) => console.error(error))
-db.once('open', () => console.log('Connected to Database'))
+mongoose.connect(process.env.DATABASE_URL);
+const client = new MongoClient(process.env.DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', (error) => console.log(error));
+db.once('open', () => console.log('Connected to database'));
 
-app.use(express.json())
+app.use(express.json());
 
-const subscribersRouter = require('./routes/subscribers')
-app.use('/subscribers', subscribersRouter)
-//localhost:3001/subscribers
+const userRouter = require('./routes/server');
+app.use('/user', userRouter);
 
-
-app.listen(3001, () => console.log('Server Started'))
+app.listen(3010, () => console.log(`Server is listening on port ${port}`));
